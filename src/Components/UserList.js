6e247e71c21a366b6b03256fd8a06b187493045d
Harshit Kumar <144579486+harshit9693@users.noreply.github.com>
2024-08-27 20:23:00 +0530

    Update UserList.js

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -27,37 +27,65 @@ import React from "react";
 import { observer } from "mobx-react-lite";
 import userStore from "../Stores/UserStore";
 
+// const UserList = observer(() => {
+//   return (
+//     <div className="user-list-container">
+//       {userStore.loading && <p>Loading users...</p>}
+//       {userStore.error && <p>{userStore.error}</p>}
+//       {!userStore.loading && !userStore.error && (
+//         <table className="user-table">
+//           <thead>
+//             <tr>
+//               <th>#</th>
+//               <th>First Name</th>
+//               <th>Last Name</th>
+//               <th>Email</th>
+//               <th>Location</th>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {userStore.paginatedUsers.map((user, index) => (
+//               <tr key={user.login.uuid}>
+//                 <td>{index + 1 + (userStore.currentPage - 1) * userStore.usersPerPage}</td>
+//                 <td>{user.name.first}</td>
+//                 <td>{user.name.last}</td>
+//                 <td>{user.email}</td>
+//                 <td>{`${user.location.city}, ${user.location.country}`}</td>
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       )}
+//     </div>
+//   );
+// });
+
 const UserList = observer(() => {
+  const { paginatedUsers } = userStore;
+
   return (
-    <div className="user-list-container">
-      {userStore.loading && <p>Loading users...</p>}
-      {userStore.error && <p>{userStore.error}</p>}
-      {!userStore.loading && !userStore.error && (
-        <table className="user-table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Location</th>
-            </tr>
-          </thead>
-          <tbody>
-            {userStore.paginatedUsers.map((user, index) => (
-              <tr key={user.login.uuid}>
-                <td>{index + 1 + (userStore.currentPage - 1) * userStore.usersPerPage}</td>
-                <td>{user.name.first}</td>
-                <td>{user.name.last}</td>
-                <td>{user.email}</td>
-                <td>{`${user.location.city}, ${user.location.country}`}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+    <table className="user-table">
+      <thead>
+        <tr>
+          <th>No</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          {/* Add other columns if needed */}
+        </tr>
+      </thead>
+      <tbody>
+        {paginatedUsers.map((user, index) => (
+          <tr key={user.login.uuid}>
+            <td>{index + 1}</td>
+            <td>{user.name.first}</td>
+            <td>{user.name.last}</td>
+            {/* Add other columns if needed */}
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 });
+
 
 export default UserList;
